a5448c0552fde3e514b2456c65c7171f85ab077b
danilojsouza <danilosouza.bm@gmail.com>
2023-05-04 16:36:07 -0300

    Resolving redirect issue

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -13,47 +13,44 @@ const routes: Routes = [
         children: [
           {
             path: '',
-            loadChildren: '../home/home.module#HomePageModule'
-          }
-        ]
+            loadChildren: '../home/home.module#HomePageModule',
+          },
+        ],
       },
       {
         path: 'tab2',
         children: [
           {
             path: '',
-            loadChildren: '../tab2/tab2.module#Tab2PageModule'
-          }
-        ]
+            loadChildren: '../tab2/tab2.module#Tab2PageModule',
+          },
+        ],
       },
       {
         path: 'tab3',
         children: [
           {
             path: '',
-            loadChildren: '../tab3/tab3.module#Tab3PageModule'
-          }
-        ]
+            loadChildren: '../tab3/tab3.module#Tab3PageModule',
+          },
+        ],
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
-      }
-    ]
+        redirectTo: '/tabs/home',
+        pathMatch: 'full',
+      },
+    ],
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
-  }
+    redirectTo: '/tabs/home',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
-  imports: [
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
-  exports: [RouterModule]
+  imports: [IonicModule, RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
 export class TabsPageRoutingModule {}
