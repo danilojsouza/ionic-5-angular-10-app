eb39822f126b39a18736215a9215449284c97c5c
danilojsouza <danilosouza.bm@gmail.com>
2023-05-04 19:53:44 -0300

    Improve whatsapp message and fix responsive buttons

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
 })
-export class Tab3Page {
-  constructor() {}
+export class Tab3Page implements OnInit {
+  form: FormGroup;
 
-  redirecionarWhatsappComMensagem() {
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      nome: ['', [Validators.required]],
+      endereco: ['', [Validators.required]],
+    });
+  }
+
+  submit() {
+    const nome = this.form.get('nome').value;
+    const endereco = this.form.get('endereco').value;
+    const mensagem = `Olá, me chamo ${nome}, moro no endereço ${endereco}. Estou precisando de doações, poderia me ajudar?`;
+    this.redirecionarWhatsappComMensagem(mensagem);
+  }
+
+  redirecionarWhatsappComMensagem(mensagem: string = 'Olá!') {
     window.open(
-      'https://api.whatsapp.com/send?phone=555194904309&text=Olá vim através do seu App. Gostaria de receber doações.',
+      `https://api.whatsapp.com/send?phone=555194904309&text=${mensagem}`,
       '_blank'
     );
   }
