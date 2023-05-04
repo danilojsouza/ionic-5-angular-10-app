b7aa5e4ca3210286c698b2db03db056f5302794d
danilojsouza <danilosouza.bm@gmail.com>
2023-05-04 19:16:45 -0300

    Add link to whatsapp default

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -3,10 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
-
   constructor() {}
 
+  redirecionarWhatsappComMensagem() {
+    window.open(
+      'https://api.whatsapp.com/send?phone=555194904309&text=Olá vim através do seu App. Gostaria de receber doações.',
+      '_blank'
+    );
+  }
 }
